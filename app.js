c7744fb0a85df2ca8c959076a7c917a6aa48909d
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2025-12-30 23:53:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working Successfully! V1');
+  res.send('CI/CD Pipeline Working Successfully!');
 });
 
 app.listen(3000, () => {
