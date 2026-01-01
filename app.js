614bca5a60e4ebc896afbc23a7eae7e0e0ed67d4
bijuthomaspta <67781816+bijuthomaspta@users.noreply.github.com>
2026-01-01 22:21:55 +0530

    Remove extra spaces in response message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working Successfully!  v1   ');
+  res.send('CI/CD Pipeline Working Successfully!');
 });
 
 app.listen(3000, () => {
